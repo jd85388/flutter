5d417c4b1ec6726ce986564336c673e18a11a959
jeferson <jd8538879@gmail.com>
2025-04-08 01:24:00 -0500

    se agregaron nuevas depedencias, se ordenaron  las carpatas del backend, se arreglaron y adjuntaron nuevos archivos backend, queda pendiente arreglar un error con req.user como extencion de Request

---
+++ life/backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import router from '../src/routes/control';
+export {};
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', router);
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.CONEXION!)
+.then(() => {
+    console.log(
+        "Conectando con la base de datos de Life reminder..."
+    );
+    app.listen(PORT, () => console.log(
+        `Life Reminder esta en conectado en  http://localhost:${PORT}`
+    ));
+})
+.catch(err => console.error(`No pudimos conectar`, err));
